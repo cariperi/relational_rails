03d179dad8f588bb36c20fd4ad616e0003f23018
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-22 17:32:01 -0400

    feat: add beers index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/breweries", to: "breweries#index"
   get "/breweries/:id", to: "breweries#show"
+
+  get "/beers", to: "beers#index"
 end
