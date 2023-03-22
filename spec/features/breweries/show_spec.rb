ac8fec6bf3e18852a6afd073034f61938915a15a
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-22 17:01:32 -0400

    test: add more expectations to brewery show spec

--- spec/features/breweries/show_spec.rb
+++ spec/features/breweries/show_spec.rb
@@ -23,4 +23,25 @@ RSpec.describe 'breweries show page', type: :feature do
     expect(page).to have_content(@brewery_1.name)
     expect(page).to_not have_content(@brewery_2.name)
   end
+
+  it 'can see the city of the specific brewery' do
+    visit "/breweries/#{@brewery_1.id}"
+
+    expect(page).to have_content(@brewery_1.city)
+    expect(page).to_not have_content(@brewery_2.city)
+  end
+
+  it 'can see the number of tanks for the specific brewery' do
+    visit "/breweries/#{@brewery_1.id}"
+
+    expect(page).to have_content(@brewery_1.tanks)
+    expect(page).to_not have_content(@brewery_2.tanks)
+  end
+
+  it 'can see whether or not the specific brewery has a license' do
+    visit "/breweries/#{@brewery_1.id}"
+
+    expect(page).to have_content(@brewery_1.has_license)
+    expect(page).to_not have_content(@brewery_2.has_license)
+  end
 end
