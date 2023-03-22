0ed336ed708443f3c39987a794ebb89b57436a71
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-22 16:36:11 -0400

    test: create feature test for parent index

---
+++ spec/features/breweries/index_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+# For each parent table
+# As a visitor
+# When I visit '/parents'
+# Then I see the name of each parent record in the system
+
+RSpec.describe 'breweries index page', type: :feature do
+  it 'can see the name of each brewery in the system' do
+    brewery_1 = Brewery.create!(name: "Talea Beer Co.",
+                                city: "New York, NY",
+                                tanks: 15,
+                                has_license: true)
+    brewery_1 = Brewery.create!(name: "Wild East",
+                                city: "New York, NY",
+                                tanks: 10,
+                                has_license: true)
+
+    visit "/breweries"
+
+    expect(page).to have_content(brewery_1.name)
+    expect(page).to have_content(brewery_2.name)
+  end
+end
