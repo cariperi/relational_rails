8272682f3aff7f1ef020acc1ccf2559563b31128
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-22 20:23:42 -0400

    feat: add order and show creation date on brewery index page

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,6 +1,6 @@
 class BreweriesController < ApplicationController
   def index
-    @breweries = Brewery.all
+    @breweries = Brewery.sort_by_creation_date
   end
 
   def show
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,7 @@
 class Brewery < ApplicationRecord
   has_many :beers
+
+  def self.sort_by_creation_date
+    Brewery.order(created_at: :desc)
+  end
 end
