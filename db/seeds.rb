7cc231953f5b25c6e9597891ef32f60e3037fa23
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-23 15:16:56 -0400

    feat: add development db data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Beer.destroy_all
+Brewery.destroy_all
+
+talea = Brewery.create!(name: "Talea Beer Co.",
+                          city: "New York, NY",
+                          tanks: 15,
+                          has_license: true)
+jester = Brewery.create!(name: "Jester King",
+                          city: "Austin, TX",
+                          tanks: 10,
+                          has_license: false)
+robot = Brewery.create!(name: "Human Robot",
+                          city: "Philadelphia, PA",
+                          tanks: 6,
+                          has_license: true)
+
+talea.beers.create!(name: "Weekender",
+                    style: "Light Lager",
+                    ibu: 7,
+                    abv: 5,
+                    fermentation_completed: true)
+talea.beers.create!(name: "Sun Up",
+                    style: "New England IPA",
+                    ibu: 38,
+                    abv: 6,
+                    fermentation_completed: false)
+
+jester.beers.create!(name: "Simple Means",
+                    style: "Altbier",
+                    ibu: 26,
+                    abv: 6,
+                    fermentation_completed: true)
+jester.beers.create!(name: "Ghost Cannon",
+                    style: "DIPA",
+                    ibu: 65,
+                    abv: 8,
+                    fermentation_completed: false)
+
+robot.beers.create!(name: "Snow Day",
+                    style: "American Stout",
+                    ibu: 42,
+                    abv: 7,
+                    fermentation_completed: false)
+robot.beers.create!(name: "Tin Hoagie",
+                    style: "Light Lager",
+                    ibu: 11,
+                    abv: 4,
+                    fermentation_completed: true)
