b90f13ca5ebbbedb0b567422987449afbdca06c2
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-23 20:06:27 -0400

    test: add test for link to parent index to application spec

--- spec/features/layouts/application_spec.rb
+++ spec/features/layouts/application_spec.rb
@@ -4,6 +4,10 @@ require 'rails_helper'
 # When I visit any page on the site
 # Then I see a link at the top of the page that takes me to the Child Index
 
+# As a visitor
+# When I visit any page on the site
+# Then I see a link at the top of the page that takes me to the Parent Index
+
 RSpec.describe 'application layout view', type: :feature do
   describe 'link to child index' do
     it 'can visit any page and see a link that goes to the beers index page' do
@@ -12,6 +16,16 @@ RSpec.describe 'application layout view', type: :feature do
 
       click_link "See All Beers"
       expect(page).to have_current_path(beers_path)
+    end
+  end
+
+  describe 'link to parent index' do
+    it 'can visit any page and see a link that goes to the breweries index page' do
+      visit('/breweries')
+      expect(page).to have_content("See All Breweries")
+
+      click_link "See All Breweries"
+      expect(page).to have_current_path(breweries_path)
     end
   end
 end
