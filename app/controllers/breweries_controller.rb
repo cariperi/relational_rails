8f059a30e57361e173971ac300c7a04b1be761a2
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-23 22:42:13 -0400

    feat: add new brewery page with form and new and create routes

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -3,8 +3,21 @@ class BreweriesController < ApplicationController
     @breweries = Brewery.sort_by_creation_date
   end
 
+  def new
+  end
+
+  def create
+    Brewery.create(brewery_params)
+    redirect_to '/breweries'
+  end
+
   def show
     @brewery = Brewery.find(params[:id])
     @count = @brewery.beers.count
+  end
+
+  private
+  def brewery_params
+    params.permit(:name, :city, :tanks, :has_license)
   end
 end
