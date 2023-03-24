19e98e9cb6c183aa1b89f8987f976e8e214bae6d
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-23 21:16:12 -0400

    test: add order expectation to application spec tests

--- spec/features/layouts/application_spec.rb
+++ spec/features/layouts/application_spec.rb
@@ -9,10 +9,18 @@ require 'rails_helper'
 # Then I see a link at the top of the page that takes me to the Parent Index
 
 RSpec.describe 'application layout view', type: :feature do
+  before(:each) do
+    @brewery_1 = Brewery.create!(name: "Talea Beer Co.",
+                                  city: "New York, NY",
+                                  tanks: 15,
+                                  has_license: true)
+  end
+
   describe 'link to child index' do
-    it 'can visit any page and see a link that goes to the beers index page' do
+    it 'can visit any page and see a link at top that goes to the beers index page' do
       visit('/breweries')
       expect(page).to have_content("See All Beers")
+      expect("See All Beers").to appear_before(@brewery_1.name)
 
       click_link "See All Beers"
       expect(page).to have_current_path(beers_path)
@@ -20,9 +28,10 @@ RSpec.describe 'application layout view', type: :feature do
   end
 
   describe 'link to parent index' do
-    it 'can visit any page and see a link that goes to the breweries index page' do
+    it 'can visit any page and see a link at top that goes to the breweries index page' do
       visit('/breweries')
       expect(page).to have_content("See All Breweries")
+      expect("See All Breweries").to appear_before(@brewery_1.name)
 
       click_link "See All Breweries"
       expect(page).to have_current_path(breweries_path)
