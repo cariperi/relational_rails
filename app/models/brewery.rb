6793a2e5d57567db6994e79d79458224abdae835
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-27 18:05:55 -0400

    feat: add ability to delete a brewery and its associated beers:

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,5 +1,5 @@
 class Brewery < ApplicationRecord
-  has_many :beers
+  has_many :beers, dependent: :destroy
 
   def self.sort_by_creation_date
     order(created_at: :desc)
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "/breweries/:id", to: "breweries#show"
   get "/breweries/:id/edit", to: "breweries#edit"
   patch "/breweries/:id", to: "breweries#update"
+  delete "/breweries/:id", to: "breweries#destroy"
 
   get "/breweries/:brewery_id/beers", to: "brewery_beers#index"
   get "/breweries/:brewery_id/beers/new", to: "brewery_beers#new"
